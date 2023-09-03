e48a2114c45fd0747e081cf15bb3cf1977d1185b
mcnoble <idowufestustemiloluwa@gmail.com>
2023-09-03 08:25:00 +0100

    feat: BinarySearch :octocat:

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,17 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let low = 0;
+    let high = haystack.length - 1;
+    let midpoint = low + (high - low) / 2;
+    let value = haystack[midpoint];
+    
+    do {    
+        if ( value === needle ) {
+            return true;
+        } else if ( needle > value ) {
+            low = midpoint + 1
+        } else {
+            high = midpoint;
+        }
+        return false;
+    } while (low < high);    
 }
